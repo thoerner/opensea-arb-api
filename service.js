eba5389a5ca3ac3a646f5d23596b5a2a3943ac6c
Tim Hoerner <thoerner@gmail.com>
2023-07-03 21:27:38 -0400

    scan every 30 sec

--- service.js
+++ service.js
@@ -2,18 +2,18 @@ import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import bodyParser from 'body-parser'
+import { v4 as uuidv4 } from 'uuid'
 import { spawn } from 'child_process'
+import { ScanCommand, PutItemCommand } from '@aws-sdk/client-dynamodb'
 import collectionRoutes from './routes/collections.js'
 import scanRoutes from './routes/scans.js'
-import { ScanCommand, PutItemCommand } from '@aws-sdk/client-dynamodb'
 import { jobs } from './jobs.js'
-import { v4 as uuidv4 } from 'uuid'
 import { dbClient } from './config/db.js'
 import { scanQueue } from './config/queue.js'
 
 const app = express()
 app.use(cors({
-    origin: '*'
+  origin: '*'
 }))
 app.use(express.json())
 app.use(bodyParser.json())
@@ -25,7 +25,7 @@ app.use('/', scanRoutes)
 
 scanQueue.process(2, (job, done) => {
   const { collectionSlug, margin, increment, schema, token } = job.data;
-  
+
   const worker = spawn('node', ['scan.js', collectionSlug, margin, increment, schema, token]);
 
   worker.stdout.on('data', (data) => {
@@ -41,80 +41,73 @@ scanQueue.process(2, (job, done) => {
     done();
   });
 });
-  
+
 app.listen(3000, async () => {
-    console.log('Server started on port 3000')
+  console.log('Server started on port 3000')
 
-    scanQueue.obliterate({ force: true })
+  scanQueue.obliterate({ force: true })
 
-    try {
-      const command = new ScanCommand({
-        TableName: 'arb_anderson_scans'
-      });
+  try {
+    const command = new ScanCommand({
+      TableName: 'arb_anderson_scans'
+    });
 
-      const data = await dbClient.send(command)
+    const data = await dbClient.send(command)
 
-      if (data.Items) {
-        for (let item of data.Items) {
-          const { slug: collectionSlug, margin, increment, schema, jobId } = item
-          let token = null
-          if (schema.S === 'ERC1155') {
-            token = item.token.S
-          } 
-
-          // if (jobId) {
-          //   const oldJob = await scanQueue.getJob(jobId.S)
-          //   if (oldJob) {
-          //     await oldJob.remove()
-          //   }
-          // }
-
-          console.log(`Resuming scan for ${collectionSlug.S}`)
-          const job = await scanQueue.add({
-            collectionSlug: collectionSlug.S,
-            margin: margin.N,
-            increment: increment.N,
-            schema: schema.S,
-            token
-          }, {
-            jobId: uuidv4(),
-            repeat: {
-              every: 3 * 60 * 1000
-            }
-          });
-
-          let dbItem = {}
-
-          if (schema.S === 'ERC1155') {
-            dbItem = {
-              slug: {S: collectionSlug.S},
-              margin: {N: margin.N},
-              increment: {N: increment.N},
-              schema: {S: schema.S},
-              jobId: {S: job.id},
-              token
-            }
-          } else if (schema.S === 'ERC721') {
-            dbItem = {
-              slug: {S: collectionSlug.S},
-              margin: {N: margin.N},
-              increment: {N: increment.N},
-              schema: {S: schema.S},
-              jobId: {S: job.id},
-            }
-          }
-
-          const putCommand = new PutItemCommand({
-            TableName: 'arb_anderson_scans',
-            Item: dbItem
-          });
-
-          await dbClient.send(putCommand)
-
-          jobs[collectionSlug.S] = job.id;
+    if (data.Items) {
+      for (let item of data.Items) {
+        const { slug: collectionSlug, margin, increment, schema } = item
+        let token = null
+        if (schema.S === 'ERC1155') {
+          token = item.token.S
         }
+
+        console.log(`Resuming scan for ${collectionSlug.S}`)
+        const job = await scanQueue.add({
+          collectionSlug: collectionSlug.S,
+          margin: margin.N,
+          increment: increment.N,
+          schema: schema.S,
+          token
+        }, {
+          jobId: uuidv4(),
+          repeat: {
+            every: 30 * 1000
+          }
+        });
+
+        let dbItem = {}
+
+        if (schema.S === 'ERC1155') {
+          dbItem = {
+            slug: { S: collectionSlug.S },
+            margin: { N: margin.N },
+            increment: { N: increment.N },
+            schema: { S: schema.S },
+            jobId: { S: job.id },
+            token
+          }
+        } else if (schema.S === 'ERC721') {
+          dbItem = {
+            slug: { S: collectionSlug.S },
+            margin: { N: margin.N },
+            increment: { N: increment.N },
+            schema: { S: schema.S },
+            jobId: { S: job.id },
+          }
+        }
+
+        const putCommand = new PutItemCommand({
+          TableName: 'arb_anderson_scans',
+          Item: dbItem
+        });
+
+        await dbClient.send(putCommand)
+
+        jobs[collectionSlug.S] = job.id;
       }
-    } catch (err) {
-      console.error(`Error resuming scans: ${err}`)
     }
+  } catch (err) {
+    console.error(`Error resuming scans: ${err}`)
+  }
 })
